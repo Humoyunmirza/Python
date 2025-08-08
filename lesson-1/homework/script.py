fa319d097fb6edc67e3c5fb549a053c89d5adabf
Humoyunmirza <humoyunirismatov@gmail.com>
2025-08-09 03:08:43 +0500

    Update script.py

--- lesson-1/homework/script.py
+++ lesson-1/homework/script.py
@@ -2,3 +2,22 @@ a = 2
 p = 4*a
 S = a*a
 print(p,S)
+
+R = 4
+П = 3.14
+L = 2*П*R
+S = П*R*R
+print(L,S)
+
+a = 4
+b = 6
+c = (a+b)/2
+print(c)
+
+a = 7
+b = 9
+c = a+b
+d = a*b
+e = a*a
+f = b*b
+print(c,d,e,f)
